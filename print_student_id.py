e1dfae0db55a4dd95ce973e8f17da91d0c197fe8
Natwakorn <124992328+Natwakorn@users.noreply.github.com>
2023-02-10 15:08:54 +0700

    egitt

--- print_student_id.py
+++ print_student_id.py
@@ -1,5 +1,7 @@
+import numpy as np 
+  def print_id(student_id):
+        print("student ID = ", student_id)
 
-def print_id(student_id):
-        print("student id = ", student_id)
-if __name__ == "__main__" :
-    print_id("6513404")
+  if __name__ == "__main__" :
+        print_id("6513404")
+        print_id("6513405")
